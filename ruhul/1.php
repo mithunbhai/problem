51dd4434b1a8be9eaf8b43e978e4fc60c515045f
Ruhulphp <raruhulcse@gmail.com>
2016-11-09 01:18:07 +0600

    added odd/oven finding code

---
+++ ruhul/1.php
@@ -0,0 +1,30 @@
+<?php
+
+
+function oddeven($num)
+{
+//while (n--)
+//$numb=5;
+	//for($i=1;$i<=$num;$i++)
+	//{
+	if($num%2 ==0)
+	  
+	    {	
+		echo "even</br>";
+	 
+	       }
+	else
+	  {
+		echo "odd</br>";
+	         }
+	
+}
+//}
+echo oddeven("10");
+echo oddeven("5");
+echo oddeven("20");
+
+
+
+?>
+
